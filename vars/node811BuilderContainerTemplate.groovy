cecc59f8c656ac8a76aff59b93843aa419451f5b
Haolin Jiang <hljiang@agiledigital.com.au>
2019-07-29 21:48:54 +1000

    Update to use cypress/base image.

--- vars/node811BuilderContainerTemplate.groovy
+++ vars/node811BuilderContainerTemplate.groovy
@@ -2,7 +2,7 @@ def call() {
   return [
     containerTemplate(
       name: 'node811-builder',
-      image: 'agiledigital/node811-builder',
+      image: 'cypress/base',
       alwaysPullImage: true,
       command: 'cat',
       ttyEnabled: true
