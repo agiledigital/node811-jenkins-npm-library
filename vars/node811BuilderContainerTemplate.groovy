90534afc6778cf6414b6937fdc8598cdecb24e6f
Robin McAndrew <rlmcandrew@agiledigital.com.au>
2020-01-17 11:22:17 +1100

    Update to work with jsdom 16
    https://github.com/jsdom/jsdom/issues/2797

--- vars/node811BuilderContainerTemplate.groovy
+++ vars/node811BuilderContainerTemplate.groovy
@@ -2,7 +2,7 @@ def call() {
   return [
     containerTemplate(
       name: 'node811-builder',
-      image: 'cypress/base',
+      image: 'cypress/base:12.14',
       alwaysPullImage: true,
       command: 'cat',
       ttyEnabled: true
